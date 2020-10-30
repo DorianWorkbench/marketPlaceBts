2a0bbd73f7e2476ff1d63d47259b3fc7f8db18b9
DorianWorkbench <58782457+DorianWorkbench@users.noreply.github.com>
2020-10-30 19:30:17 +0100

    Update co.php

--- fonctions_php/co.php
+++ fonctions_php/co.php
@@ -1,11 +1,7 @@
 <?php
-    // try{
-    //     $c = new PDO("sqlsrv:Server=DESKTOP-CJC5BPT\SQLEXPRESS;Database=test2", "sa", "tennis");
-    // }catch(Exception $e){
-    //     echo "Erreur lors de la connexion";
-    // }
+    
     try {
-        $c = new PDO("sqlsrv:server = tcp:b9al9h5psh.database.windows.net,1433; Database = marketplaceApi", "brebisepa", "Poncho2013*");
+        $c = new PDO("connexion à votre base de donnée");
         $c->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $e) {
